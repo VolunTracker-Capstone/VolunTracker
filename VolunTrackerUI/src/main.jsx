c0bb0453e0b29ddc262a50ab370cb6fc3ac3564a
jrizo19 <89281518+jrizo19@users.noreply.github.com>
2024-01-21 22:10:41 -0600

    Updated main.jsx

--- VolunTrackerUI/src/main.jsx
+++ VolunTrackerUI/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 
-const root = document.getElementById('root');
-ReactDOM.render(
-    // <React.StrictMode>
-        <App />,
-    // </React.StrictMode>,
-    root
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
